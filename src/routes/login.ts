2296453d7bfdd5f0768d5abe708c559b2b64211d
Koxanybak <leevi.heikkila11@gmail.com>
2020-09-18 22:28:06 +0300

    auth

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -5,15 +5,20 @@ import { getUserByName, getUserFromToken } from "../services/user-service"
 import { compare } from "bcrypt"
 import jwt from "jsonwebtoken"
 import { REFRESH_COOKIE_NAME, DEFAULT_TOKEN_EXP_SEC, SECRET } from "../utils/config"
-import { AES } from "crypto-js";
+import { AES, enc } from "crypto-js";
 
 const loginRouter = Router()
 
 loginRouter.get("/", expressAsyncHandler(async (req: Request, res: Response) => {
   const refresh_token = req.cookies[REFRESH_COOKIE_NAME]
   if (!refresh_token) res.status(401).send({ error: "Refresh token missing." })
-  const logged_user = await getUserFromToken(AES.decrypt(refresh_token, SECRET).toString())
-  res.status(200).json(logged_user)
+  const access_token = AES.decrypt(refresh_token, SECRET).toString(enc.Utf8)
+  const logged_user = await getUserFromToken(access_token)
+  res.status(200).json({
+    id: logged_user.id,
+    username: logged_user.username,
+    access_token,
+  })
 }))
 
 loginRouter.post("/", expressAsyncHandler(async (req: Request, res: Response) => {
@@ -31,7 +36,7 @@ loginRouter.post("/", expressAsyncHandler(async (req: Request, res: Response) =>
 
     const refresh_token = AES.encrypt(access_token, SECRET).toString()
 
-    res.cookie(REFRESH_COOKIE_NAME, refresh_token, { sameSite: true, maxAge: DEFAULT_TOKEN_EXP_SEC })
+    res.cookie(REFRESH_COOKIE_NAME, refresh_token, { sameSite: true, maxAge: DEFAULT_TOKEN_EXP_SEC*1000 })
     res.status(200).json({ ...userForToken, access_token, })
   }
 }))
